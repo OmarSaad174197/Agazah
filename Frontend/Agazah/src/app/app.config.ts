eaf41b2925a07e3d45d46fe863d58bbd55bc4c68
OmarSaad1741972 <omarsolimanne@gmail.com>
2026-09-05 02:29:43 +0300

    Enhance the dialog message to be In arabic.

--- Frontend/Agazah/src/app/app.config.ts
+++ Frontend/Agazah/src/app/app.config.ts
@@ -1,34 +1,66 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import { routes } from './app.routes';
-import {errorInterceptor} from './core/interceptors/error.interceptor';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideZoneChangeDetection
+} from '@angular/core';
+
+import {
+  provideRouter
+} from '@angular/router';
+
+import {
+  provideHttpClient,
+  withInterceptors
+} from '@angular/common/http';
+
+import {
+  registerLocaleData
+} from '@angular/common';
+
+import localeAr from '@angular/common/locales/ar';
+
 import {
   MatPaginatorIntl
-}
-from '@angular/material/paginator';
+} from '@angular/material/paginator';
+
+import {
+  routes
+} from './app.routes';
+
+import {
+  errorInterceptor
+} from './core/interceptors/error.interceptor';
 
 import {
   getArabicPaginator
-}
-from './core/services/paginator-arabic.service';
+} from './core/services/paginator-arabic.service';
+
+registerLocaleData(localeAr);
+
 export const appConfig: ApplicationConfig = {
+
   providers: [
 
-  provideZoneChangeDetection(
-    {
+    provideZoneChangeDetection({
       eventCoalescing: true
     }),
 
-  provideRouter(routes),
+    provideRouter(routes),
 
-  provideHttpClient(
-      withInterceptors([errorInterceptor])
+    provideHttpClient(
+      withInterceptors([
+        errorInterceptor
+      ])
     ),
-  {
-    provide: MatPaginatorIntl,
-    useValue:
-      getArabicPaginator()
-  }
-]
+
+    {
+      provide: LOCALE_ID,
+      useValue: 'ar-EG'
+    },
+
+    {
+      provide: MatPaginatorIntl,
+      useValue: getArabicPaginator()
+    }
+  ]
 };
